df36bd605b57ebb32de91df7dc834cf967b71803
Pavel Fleisher <pavel.fleisher@gmail.com>
2022-02-01 23:08:42 +0200

    Fixed fonts, moved head to head.blade.php

---
+++ resources/views/head.blade.php
@@ -0,0 +1,23 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>DonationBox</title>
+
+<!-- Fonts -->
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Space+Grotesk:wght@300;400;500;600;700&display=swap');
+</style>
+
+<!-- Styles -->
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}" defer></script>
+<script src="//unpkg.com/alpinejs" defer></script>
+
+<style>
+    body {
+        font-family: 'Space Grotesk', sans-serif;
+    }
+</style>
