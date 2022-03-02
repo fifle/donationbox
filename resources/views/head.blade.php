71e9ab6aec667001a0fa99a984957ee1b5e66311
Pavel Fleisher <pavel.fleisher@gmail.com>
2022-03-02 16:23:17 +0200

    Removed redundant share.js file.

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -45,7 +45,6 @@
 <!-- Scripts -->
 <script src="{{ asset('js/app.js') }}" defer></script>
 <script src="//unpkg.com/alpinejs" defer></script>
-<script src="{{ asset('js/share.js') }}"></script>
 <script src="//kit.fontawesome.com/6940ba20ce.js" crossorigin="anonymous"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.8/clipboard.min.js"></script>
 
