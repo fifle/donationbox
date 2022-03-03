e8ee502f66b5742b33ef1fa67da83160cdad77a3
Pavel Fleisher <pavel.fleisher@gmail.com>
2022-03-03 17:48:28 +0200

    Blades refactoring

--- resources/views/embed.blade.php
+++ resources/views/embed.blade.php
@@ -17,32 +17,5 @@
 
 </div>
 
-<script>
-    function app() {
-        return {
-            step: 1,
-            passwordStrengthText: '',
-            togglePassword: false,
-
-            password: '',
-            gender: 'Male',
-
-            checkPasswordStrength() {
-                var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
-                var mediumRegex = new RegExp("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})");
-
-                let value = this.password;
-
-                if (strongRegex.test(value)) {
-                    this.passwordStrengthText = "Strong password";
-                } else if (mediumRegex.test(value)) {
-                    this.passwordStrengthText = "Could be stronger";
-                } else {
-                    this.passwordStrengthText = "Too weak";
-                }
-            }
-        }
-    }
-</script>
 </body>
 </html>
