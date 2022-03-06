aa30c253a9bb754e5be4bc433c55004e7003e0dc
Pavel Fleisher <pavel.fleisher@gmail.com>
2022-03-06 02:09:04 +0200

    Report fraud button added.

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,4 +1,20 @@
-<div class="pt-8 pb-16 text-center text-gray-600">
+<div class="pt-8 pb-14 text-center text-gray-600">
+
+    @if(Route::currentRouteName() === 'donation')
+    <a href="mailto:donationbox.ee@gmail.com?subject=Report%20Alert%20from%20Donationbox.ee&body=Your%20name%3A%0D%0AYour%20email%3A%0D%0AReason%20for%20reporting%3A%0D%0AReported%20URL%3A%20{{
+    urlencode(url()->full()) }}"
+       target="_blank"
+       class="transition duration-150 ease-in-out bg-white px-5
+                                                py-2 mb-4 text-sm shadow-sm font-medium
+                                                     tracking-wider border text-red-400 rounded-full hover:shadow-lg
+                                                     hover:bg-gray-50 inline-flex items-center">
+
+        <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3
+        .org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path></svg>
+        Report fraud
+    </a>
+    @endif
+
     <div class="flex justify-center mb-2 text-xs">
         <a href="#" class="no-underline hover:underline text-blue-800" target="_blank">
             FAQ
@@ -28,7 +44,7 @@
     </a>
     and inspired by Sheila
     <br>
-    <a href="/donation?campaign_title=Support+Donationbox.ee&detail=Annetus+donationbox.ee&payee=Pavel+Flei%C5%A1er&iban=EE614204278622417401&pp=pfleiser" class="no-underline
+    <a href="/donation?campaign_title=Support+Donationbox.ee&detail=Annetus+donationbox.ee&payee=Pavel+Flei%C5%A1er&iban=EE614204278622417401&pp=pfleiser&rev=pavelvtd" class="no-underline
                  hover:underline text-xs text-blue-800 mt-4" target="_blank">
         <div class="inline-flex items-center mt-2">
             Support the project <svg class="w-3 h-3" fill="none" stroke="currentColor" viewBox="0 0 24 24"
