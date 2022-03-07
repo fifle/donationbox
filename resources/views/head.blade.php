d5081d76f54bec4227234a573f754ad18aaf51d6
Pavel Fleisher <pavel.fleisher@gmail.com>
2022-03-07 18:20:00 +0200

    Added og:image.

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -1,24 +1,23 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-
 <meta name="description" content="@yield('description', config('app.description'))"/>
 <meta name="keywords" content="@yield('keywords', config('app.keywords'))"/>
 <meta name="copyright" content="{{ config('app.name') }}">
 <meta name="author" content="{{ config('app.name') }}"/>
 <meta name="application-name" content="@yield('title', config('app.name'))">
 <!--Facebook Tags-->
-<meta property="og:type" content="article"/>
+<meta property="og:type" content="website"/>
 <meta property="og:url" content="{{ request()->fullUrl() }}"/>
 <meta property="og:description" content="@yield('description', config('app.description'))"/>
-<meta property="og:image" content="{{ request()->root() }}/images/TODO.png"/>
-<meta property="article:author" content="https://www.facebook.com/TODO"/>
+<meta property="og:image" content="{{ request()->root() }}/img/db-social.png"/>
+<meta property="article:author" content=""/>
 <meta property="og:locale" content="en_UK"/>
 <!--Twitter Tags-->
 <meta name="twitter:card" content="summary"/>
 <meta name="twitter:site" content="{{ '@' . config('app.name') }}"/>
 <meta name="twitter:title" content="@yield('title', config('app.name'))"/>
 <meta name="twitter:description" content="@yield('description', config('app.description'))"/>
-<meta name="twitter:image" content="{{ request()->root() }}/images/TODO.png"/>
+<meta name="twitter:image" content="{{ request()->root() }}/img/db-social.png"/>
 
 <title>DonationBox</title>
 
