b7bc4ee9f401765e286f33af61c28e9b02e7a503
Pavel Fleisher <pavel.fleisher@gmail.com>
2022-03-08 18:14:06 +0200

    SEO changes.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <title>Start your virtual donation box for Estonian banks for free and without any hidden fees - DonationBox
-        .ee</title>
+    <title>Start your virtual donation box for Estonian banks for free and without any hidden fees - DonationBox.ee</title>
     @include('head')
 </head>
 <body class="antialiased">
