43bab80e06ba7b7277254d08d768b9f8fafc8bea
Pavel Fleiser <pavel.fleiser@printful.com>
2022-03-17 14:54:30 +0200

    IBAN validation for form input field.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -174,6 +174,7 @@
                                                 type="text"
                                                 name="iban"
                                                 value="{{ request('iban') }}"
+                                                pattern="^(?:(?:IT|SM)\d{2}[A-Z]\d{22}|CY\d{2}[A-Z]\d{23}|NL\d{2}[A-Z]{4}\d{10}|LV\d{2}[A-Z]{4}\d{13}|(?:BG|BH|GB|IE)\d{2}[A-Z]{4}\d{14}|GI\d{2}[A-Z]{4}\d{15}|RO\d{2}[A-Z]{4}\d{16}|KW\d{2}[A-Z]{4}\d{22}|MT\d{2}[A-Z]{4}\d{23}|NO\d{13}|(?:DK|FI|GL|FO)\d{16}|MK\d{17}|(?:AT|EE|KZ|LU|XK)\d{18}|(?:BA|HR|LI|CH|CR)\d{19}|(?:GE|DE|LT|ME|RS)\d{20}|IL\d{21}|(?:AD|CZ|ES|MD|SA)\d{22}|PT\d{23}|(?:BE|IS)\d{24}|(?:FR|MR|MC)\d{25}|(?:AL|DO|LB|PL)\d{26}|(?:AZ|HU)\d{27}|(?:GR|MU)\d{28})$"
                                                 class="appearance-none rounded-none relative block
                                                                w-full px-3 py-2 border border-gray-300
                                                                text-gray-900 rounded-md
