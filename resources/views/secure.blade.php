6be76eb343cedc73ece07880e94ef16adbdfa4f6
Pavel Fleiser <pavel.fleiser@printful.com>
2022-04-28 14:24:39 +0300

    Secure banner color fixes.

--- resources/views/secure.blade.php
+++ resources/views/secure.blade.php
@@ -1,8 +1,8 @@
-<div class="w-full mb-4 flex justify-center items-center text-left">
+<div class="w-full mb-4 flex justify-center items-center text-left text-gray-500">
     <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"
          xmlns="http://www.w3
                     .org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"></path></svg>
-    <a href="/about#security" class="d-font inline-block items-center no-underline hover:underline text-xs text-gray-400
+    <a href="/about#security" class="d-font inline-block items-center no-underline hover:underline text-xs
                 text-left" target="_blank">
         It's secure and we won't store your data.
         <br>Learn more about how it works >
