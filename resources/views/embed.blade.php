121dd67d7b8fba8b88b1bf02b83fde4c0ed3b847
Pavel Fleiser <pavel.fleiser@printful.com>
2022-07-13 12:53:30 +0300

    Changes in ee and ru translations.

--- resources/views/embed.blade.php
+++ resources/views/embed.blade.php
@@ -6,19 +6,28 @@
 </head>
 <body class="antialiased">
 <div class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-    <div class="max-w-md w-full space-y-2">
-        <div class="grid grid-cols-2 items-center mb-4">
-            <div class="w-32">
-                <a href="/" target="_blank">
-                    <img class="mx-auto" src="/img/db-logo-fl-{{ env('COUNTRY') }}.png">
-                </a>
-            </div>
-            <div class="">
-                @include('components.lang-switcher')
-            </div>
-        </div>
+{{--    <div class="max-w-md w-full space-y-2">--}}
+{{--        <div class="grid grid-cols-2 items-center mb-4">--}}
+{{--            <div class="w-32">--}}
+{{--                <a href="/" target="_blank">--}}
+{{--                    <img class="mx-auto" src="/img/db-logo-fl-{{ env('COUNTRY') }}.png">--}}
+{{--                </a>--}}
+{{--            </div>--}}
+{{--            <div class="">--}}
+{{--                @include('components.lang-switcher')--}}
+{{--            </div>--}}
+{{--        </div>--}}
 
+{{--        @include('form')--}}
+{{--    </div>--}}
+
+    <div class="max-w-md w-full space-y-2">
         @include('form')
+        <div class="mt-8 w-1/4 mx-auto">
+            <a href="/" target="_blank">
+                <img class="mx-auto" src="/img/db-logo-fl-{{ env('COUNTRY') }}.png">
+            </a>
+        </div>
     </div>
 
 </div>
